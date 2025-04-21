2200a5448c110c09d61836cce07b5b83dd31f842
EL HAIMER <sara.el-haimer@edu.dsti.institute>
2025-04-21 17:11:48 +0200

    added functionality to DVC

---
+++ cybulde/utils/data_utils.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+from cybulde.utils.utils import get_logger, run_shell_command
+
+
+DATA_UTILS_LOGGER = get_logger(Path(__file__).name)
+
+def is_dvc_initialized() -> bool:
+    return (Path().cwd() / ".dvc").exists()
+
+def initialize_dvc() -> None:
+    DATA_UTILS_LOGGER.info("DVC is already initialized")
+    if is_dvc_initialized():
+        return
+    
+    DATA_UTILS_LOGGER.info("Initializing DVC")
+    run_shell_command("dvc init")
+    run_shell_command("dvc config core.analytics false")
+    run_shell_command("dvc config core.autostage true")
+    run_shell_command("git add .dvc")
+    run_shell_command("git commit -nm 'Initialized DVC'")
